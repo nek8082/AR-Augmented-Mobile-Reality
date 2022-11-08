be3a9a49ee3e2527da378ef66920dd53514f7c38
Nelson Kapoor <nelson.kapoor@stud.uni-due.de>
2022-11-08 19:41:19 +0100

    Limit amount of robots to 5

--- src/Assets/ARController.cs
+++ src/Assets/ARController.cs
@@ -7,25 +7,32 @@ public class ARController : MonoBehaviour
 {
     public GameObject robotObject;
     public ARRaycastManager RaycastManager;
+    public int amountOfRobots = 5;
     
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        Debug.Log("Amount of screen touches: " + Input.touchCount);
+        
+        if (Input.touchCount > 0)
         {
-            List<ARRaycastHit> touches = new List<ARRaycastHit>();
-            RaycastManager.Raycast(Input.GetTouch(0).position, touches,
-                UnityEngine.XR.ARSubsystems.TrackableType.Planes);
-
-            if (touches.Count > 0)
+            Touch touch = Input.GetTouch(0);
+            
+            if (touch.phase == TouchPhase.Began)
             {
-                GameObject.Instantiate(robotObject, touches[0].pose.position, touches[0].pose.rotation);
+                List<ARRaycastHit> touches = new List<ARRaycastHit>();
+                RaycastManager.Raycast(touch.position, touches, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+                
+                if (touches.Count > 0 && amountOfRobots > 0)
+                {
+                    amountOfRobots = amountOfRobots - 1;
+                    GameObject.Instantiate(robotObject, touches[0].pose.position, touches[0].pose.rotation);
+                }
             }
         }
     }
